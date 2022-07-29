f516ed6307ab83927bea508c38e9b25234bb4e0b
ahtar <ahtar1234@gmail.com>
2022-07-29 11:58:42 +0900

    Update image search

--- src/composables/images.ts
+++ src/composables/images.ts
@@ -12,37 +12,59 @@ export default function () {
     let tagsRef = ref<Array<string>>([]);
 
     /**
-     * ComputedRef с изображениями, содержащими введенные пользователем теги.
+     * ComputedRef с изображениями, соответствующими запросу пользователя.
      */
     const filteredImages = computed(() => {
         if (tagsRef.value.length == 0) return images.value;
-        else {
-            return images.value!.filter((image) => {
-                if ("arr" in image) {
-                    for (const t of image.arr) {
-                        for (const tag of tagsRef.value) {
-                            if (!t.manifest.tags.includes(tag)) {
-                                return false;
-                            }
-                        }
-                    }
-                } else {
+        return images.value!.filter((image) => {
+
+            /**
+             * Проверка каждого изображения в сете на наличие требуемых тегов.
+             * Если хотя бы 1 изображение в сете соответствует требованиям,
+             * то сет отображается.
+             */
+            if ("arr" in image) {
+                let includes = false;
+                imageLoop:
+                for (const t of image.arr) {
                     for (const tag of tagsRef.value) {
-                        if (/!.+/.test(tag)) {
-                            if (image.manifest.tags.includes(tag.replace("!", ""))) {
-                                return false;
-                            }
-                        } else {
-                            if (!image.manifest.tags.includes(tag)) {
-                                return false;
-                            }
+                        const a = /!.+/.test(tag);
+
+                        //Если тег начинается с ! и изображение содержит этот тег,
+                        //то весь сет не подходит под запрос.
+                        if (a && t.manifest.tags.includes(tag.replace("!", ""))) {
+                            includes = false;
+                            break imageLoop;
+                        }
+
+                        //Если тег не начинается с ! и изображение не содержит этот тег,
+                        //то изображение не подходит под запрос.
+                        if (!a && !t.manifest.tags.includes(tag)) {
+                            continue imageLoop;
                         }
                     }
+
+                    includes = true;
+                    //break imageLoop;
                 }
 
-                return true;
-            });
-        }
+                return includes;
+            }
+
+            for (const tag of tagsRef.value) {
+                const a = /!.+/.test(tag);
+
+                //Если тег начинается с ! и изображение содержит этот тег,
+                //то изображение не подходит под запрос.
+                if (a && image.manifest.tags.includes(tag.replace("!", ""))) return false;
+
+                //Если тег не начинается с ! и изображение не содержит этот тег,
+                //то изображение не подходит под запрос.
+                if (!a && !image.manifest.tags.includes(tag)) return false;
+            }
+
+            return true;
+        });
     });
 
     /**
