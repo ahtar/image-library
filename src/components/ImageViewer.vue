956fdfc10352762409d9fe4612349fdbcfea2804
ahtar <ahtar1234@gmail.com>
2022-09-08 11:00:28 +0900

    Fix scroll bug in imageViewer

--- src/components/ImageViewer.vue
+++ src/components/ImageViewer.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="image-wrapper" @mouseup="moveEnd" @mousemove="move" ref="imageWrapper">
+    <div class="image-viewer-wrapper wrapper" @mouseup="moveEnd" @mousemove="move" ref="imageWrapper">
         <image-vue :data="imgData" alt="Image" @wheel="scale" @mousedown="moveStart"/>
     </div>
 </template>
@@ -158,7 +158,7 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-.image-wrapper {
+.image-viewer-wrapper {
     max-height: inherit;
     max-width: inherit;
 
