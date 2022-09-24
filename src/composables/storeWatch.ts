97ea1b478638cf2ef37ba588a0668c38fc836664
ahtar <ahtar1234@gmail.com>
2022-09-24 18:38:41 +0900

    Create composable with settings watchers

---
+++ src/composables/storeWatch.ts
@@ -0,0 +1,38 @@
+import { watch } from 'vue';
+import { useSettings } from '@/store/settings';
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export default function () {
+    const storeSettings = useSettings();
+
+    function watchSettings() {
+        //выбранный пользователем язык
+        watch(
+            () => storeSettings.language.appLanguage,
+            (data) => {
+                storeSettings.saveSettings('language', data);
+            }
+        );
+
+        //Показ tooltip
+        watch(
+            () => storeSettings.showTooltips,
+            (data) => {
+                storeSettings.saveSettings('tooltips', data);
+            }
+        );
+
+        watch(
+            () => storeSettings.directoryHandle,
+            (data) => {
+                if (data) {
+                    storeSettings.saveSettings('directoryHandle', data);
+                }
+            }
+        );
+    }
+
+    return {
+        watchSettings,
+    };
+}
