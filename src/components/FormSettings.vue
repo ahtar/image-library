71c4791cf75825ffd2776772afa4b43758488a03
ahtar <ahtar1234@gmail.com>
2022-09-24 15:23:08 +0900

    Add settings form

---
+++ src/components/FormSettings.vue
@@ -0,0 +1,48 @@
+<template>
+    <modal-dark @close="close">
+        <div class="form-settings-wrapper">
+            <input-checkbox
+                v-model="store.showTooltips"
+                :label="t('LABEL.SHOW_TOOLTIPS')"
+            />
+        </div>
+    </modal-dark>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import ModalDark from './ModalDark.vue';
+import InputCheckbox from './InputCheckbox.vue';
+
+import { useSettings } from '@/store/settings';
+import { useI18n } from 'vue-i18n';
+
+export default defineComponent({
+    components: {
+        ModalDark,
+        InputCheckbox,
+    },
+    setup() {
+        const store = useSettings();
+        const { t } = useI18n();
+
+        function close() {
+            store.visible = false;
+        }
+
+        return {
+            store,
+            close,
+            t,
+        };
+    },
+});
+</script>
+
+<style lang="scss" scope>
+.form-settings-wrapper {
+    padding: 1.5vh;
+    @include material(1);
+}
+</style>
