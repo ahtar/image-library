8c783be2605657dcc10b88c5e6fb65f129d528be
ahtar <ahtar1234@gmail.com>
2022-09-28 16:56:47 +0900

    Add sidebar button

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,17 @@
 <template>
     <header class="header">
-        <div class="section">
-            <input-select-list
-                :data="store.language.languageData"
-                :model-value="store.language.appLanguage"
-                @update:model-value="store.changeLanguage"
-            />
+        <div class="section section-left" v-if="storeCollections.activeCollection != null"
+            @click="storeSettings.collectionSidebarVisible = !storeSettings.collectionSidebarVisible">
+            <button-small class="button-settings">
+                <img src="@/assets/show-sidebar.svg" />
+            </button-small>
         </div>
         <div class="section">
-            <button-small
-                @click="showSettings"
-                class="button-settings"
-                ref="buttonSettings"
-            >
+            <input-select-list :data="storeSettings.language.languageData"
+                :model-value="storeSettings.language.appLanguage" @update:model-value="storeSettings.changeLanguage" />
+        </div>
+        <div class="section">
+            <button-small @click="showSettings" class="button-settings" ref="buttonSettings">
                 <img src="@/assets/settings.svg" />
             </button-small>
         </div>
@@ -25,6 +24,7 @@ import { ComponentPublicInstance, defineComponent, ref } from 'vue';
 import InputSelectList from './InputSelectList.vue';
 import ButtonSmall from './ButtonSmall.vue';
 import { useSettings } from '@/store/settings';
+import { useCollections } from '@/store/collections'
 
 export default defineComponent({
     components: {
@@ -33,23 +33,25 @@ export default defineComponent({
     },
 
     setup() {
-        const store = useSettings();
+        const storeSettings = useSettings();
+        const storeCollections = useCollections();
         const buttonSettings = ref<ComponentPublicInstance>();
 
         function showSettings() {
-            store.visible = true;
+            storeSettings.visible = true;
 
             if (buttonSettings.value) buttonSettings.value.$el.blur();
         }
 
         return {
-            store,
+            storeSettings,
+            storeCollections,
             buttonSettings,
             showSettings,
         };
     },
 });
-</script>
+</script> 
 
 <style lang="scss" scoped>
 .header {
@@ -64,6 +66,11 @@ export default defineComponent({
         margin-right: 20px;
         height: 100%;
         @include flex-center-vertical();
+    }
+
+    .section-left {
+        margin-right: auto;
+        margin-left: 20px;
     }
 
     .button-settings {
