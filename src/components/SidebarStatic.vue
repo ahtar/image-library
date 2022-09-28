448fc1ef198654995314e60e7960a13b183d9273
ahtar <ahtar1234@gmail.com>
2022-09-28 16:49:53 +0900

    Add new sidebar component

---
+++ src/components/SidebarStatic.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="sidebar-wrapper">
+        <slot />
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    setup() {
+        return {};
+    },
+});
+</script>
+
+<style lang="scss" scoped>
+.sidebar-wrapper {
+    z-index: 1;
+    width: 10vw;
+    flex-shrink: 0;
+    padding-top: 1vh;
+    padding: 1vh 1vw 0 1vw;
+    @include material(1);
+}
+
+::v-slotted(*) {
+    margin-bottom: 2vh;
+}
+</style>
